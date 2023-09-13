3d66514127ea261f78fe308b00ee9b2eda674304
Naohiro Yoshida <naohiro.y@gmail.com>
2023-09-13 14:51:57 +0900

    Use base_fee_per_gas (#23)
    * use base_fee_per_gas if exists
    
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- light-client/src/header/eth_header.rs
+++ light-client/src/header/eth_header.rs
@@ -271,8 +271,14 @@ impl TryFrom<&RawETHHeader> for ETHHeader {
             return Err(Error::UnexpectedNonce(number));
         }
 
+        let base_fee_per_gas: Option<u64> = rlp.try_next_as_val().map(Some).unwrap_or(None);
+
         // create block hash
-        let mut stream = RlpStream::new_list(15);
+        let mut size = 15;
+        if base_fee_per_gas.is_some() {
+            size += 1;
+        }
+        let mut stream = RlpStream::new_list(size);
         stream.append(&parent_hash);
         stream.append(&uncle_hash);
         stream.append(&coinbase);
@@ -288,6 +294,11 @@ impl TryFrom<&RawETHHeader> for ETHHeader {
         stream.append(&extra_data);
         stream.append(&mix_digest);
         stream.append(&nonce);
+
+        //https://github.com/bnb-chain/bsc/blob/bb6bdc055d1a7f1f049c924028ad8aaf04291b3b/core/types/gen_header_rlp.go#L43
+        if let Some(v) = base_fee_per_gas {
+            stream.append(&v);
+        }
         let buffer_vec: Vec<u8> = stream.out().to_vec();
         let hash: Hash = keccak_256(&buffer_vec);
 
