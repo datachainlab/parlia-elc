75b2a55539cd5b3255b72bf38913a81f9bdf04bf
Naohiro Yoshida <naohiro.yoshida@datachain.jp>
2024-11-12 19:48:05 +0900

    BELC1
    Signed-off-by: Naohiro Yoshida <naohiro.yoshida@datachain.jp>

--- light-client/src/errors.rs
+++ light-client/src/errors.rs
@@ -80,11 +80,10 @@ pub enum Error {
     MissingTurnLengthInEpochBlock(BlockNumber),
     MissingEpochInfoInEpochBlock(BlockNumber),
     MissingNextValidatorSet(BlockNumber),
-    MissingCurrentValidatorSet(BlockNumber),
     UnexpectedPreviousValidatorsHash(Height, Height, Hash, Hash),
     UnexpectedCurrentValidatorsHash(Height, Height, Hash, Hash),
     InvalidVerifyingHeaderLength(BlockNumber, usize),
-    InsufficientTrustedValidatorsInUntrustedValidators(Hash, usize, usize),
+    InsufficientHonestValidator(Hash, usize, usize),
     MissingValidatorToVerifySeal(BlockNumber),
     MissingValidatorToVerifyVote(BlockNumber),
     UnexpectedNextCheckpointHeader(BlockNumber, BlockNumber),
@@ -94,6 +93,7 @@ pub enum Error {
     UnexpectedDifficultyNoTurn(BlockNumber, u64, usize),
     UnexpectedUntrustedValidatorsHashInEpoch(Height, Height, Hash, Hash),
     UnexpectedCurrentValidatorsHashInEpoch(Height, Height, Hash, Hash),
+    UnexpectedUntrustedValidators(BlockNumber, BlockNumber),
 
     // Vote attestation
     UnexpectedTooManyHeadersToFinalize(BlockNumber, usize),
@@ -309,18 +309,11 @@ impl core::fmt::Display for Error {
             Error::UnexpectedVoteRelation(e1, e2, e3) => {
                 write!(f, "UnexpectedVoteRelation : {} {} {:?}", e1, e2, e3)
             }
-            Error::InsufficientTrustedValidatorsInUntrustedValidators(e1, e2, e3) => {
-                write!(
-                    f,
-                    "InsufficientTrustedValidatorsInUntrustedValidators : {:?} {} {}",
-                    e1, e2, e3
-                )
+            Error::InsufficientHonestValidator(e1, e2, e3) => {
+                write!(f, "InsufficientHonestValidator : {:?} {} {}", e1, e2, e3)
             }
             Error::MissingNextValidatorSet(e1) => {
                 write!(f, "MissingNextValidatorSet : {}", e1)
-            }
-            Error::MissingCurrentValidatorSet(e1) => {
-                write!(f, "MissingCurrentValidatorSet : {}", e1)
             }
             Error::MissingValidatorToVerifySeal(e1) => {
                 write!(f, "MissingValidatorToVerifySeal : {:?}", e1)
@@ -371,6 +364,9 @@ impl core::fmt::Display for Error {
                     "UnexpectedCurrentValidatorsHashInEpoch : {:?} {:?} {:?} {:?}",
                     e1, e2, e3, e4
                 )
+            }
+            Error::UnexpectedUntrustedValidators(e1, e2) => {
+                write!(f, "UnexpectedUntrustedValidators : {} {}", e1, e2)
             }
         }
     }
